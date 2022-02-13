204a036cd1e0ec4b08aff23fbec055a10f53b152
alswhd20 <alswhd20@gmail.com>
2022-02-13 18:41:05 +0900

    스프링 배치 도메인 이해-JOB

--- src/main/java/io/springbatch/JobConfiguration.java
+++ src/main/java/io/springbatch/JobConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class DBJobConfiguration {
+public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
